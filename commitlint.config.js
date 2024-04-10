5e26e71aec65999997f1cf047554129b3426e4fc
Mergin <none@none>
2024-04-10 21:13:50 +0200

    chore: added commitlint rules with husky

---
+++ commitlint.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'header-max-length': [1, 'always', 72],
+        'type-enum': [
+            2,
+            'always',
+            [
+                'chore',
+                'ci',
+                'docs',
+                'feat',
+                'fix',
+                'perf',
+                'refactor',
+                'revert',
+                'style',
+                'test'
+            ]
+        ]
+    }
+};
