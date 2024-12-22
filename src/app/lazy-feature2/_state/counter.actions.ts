1f4d21e125a3a76fa308b08f03e66147dacf7bc8
Mergin <none@none>
2024-12-22 17:38:17 +0100

    chore: dependencies bumps

--- src/app/lazy-feature2/_state/counter.actions.ts
+++ src/app/lazy-feature2/_state/counter.actions.ts
@@ -1,5 +1,8 @@
 import { createAction } from '@ngrx/store';
 
+export const ACTIONS = ['increment', 'decrement', 'reset'] as const;
+export type Action = typeof ACTIONS[number];
+
 export const actions = {
     increment: createAction('[Counter Feature] Increment'),
     decrement: createAction('[Counter Feature] Decrement'),
